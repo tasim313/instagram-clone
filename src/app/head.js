ebc7b152eb357464c7396b410be41ba092ff406b
tasim313 <mostasimmahmudtasim@gmail.com>
2023-11-01 13:36:06 +0600

    first commit

---
+++ src/app/head.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Head = () =>{
+
+    return(
+        <>
+            <title>Instageram App Clone</title>
+            <meta content="width=device-width, initial-scale=1" name="viewport" />
+            <meta name="description" content="Google clone created by Next js" />
+            <link rel="icon" href="favicon.ico" />
+        </>
+    )
+}
+
+export default Head;
