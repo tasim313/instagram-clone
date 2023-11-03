e7c4b989d1df11b8ebf8f09c727c34d54e55b540
tasim313 <mostasimmahmudtasim@gmail.com>
2023-11-03 19:37:07 +0600

    make the feed section responsive

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -6,8 +6,8 @@ const Feed = () =>{
    
     return(
         <>
-        <main>
-            <section>
+        <main className='grid grid-cols-1 md:grid-cols-3 md:max-w-6xl mx-auto'>
+            <section className='md:col-span-2'>
 
                 {/* Stories */}
                 <Stories/>
@@ -15,7 +15,7 @@ const Feed = () =>{
                 {/* Posts */}
                 <Posts/>
             </section>
-            <section>
+            <section className='hidden md:inline-grid md:col-span-1'>
 
                 {/* mini profile */}
                   
